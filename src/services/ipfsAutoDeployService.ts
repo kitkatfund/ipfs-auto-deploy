e9cc008625970cbd6719c8397d007e9d11291a8c
Saurabh Tripathi <tripsaurabh1994@gmail.com>
2024-02-10 15:39:35 +0530

    g

--- src/services/ipfsAutoDeployService.ts
+++ src/services/ipfsAutoDeployService.ts
@@ -1,6 +1,7 @@
 import { GithubService } from "./githubService.js";
 import { FastifyInstance } from "fastify";
 import { IPFSDeploymentDetailsRepository } from "../repository/ipfsDeploymentDetailsRepository.js";
+import { Octokit } from "octokit";
 
 export class IPFSAutoDeployService {
     private githubService: GithubService;
@@ -11,6 +12,23 @@ export class IPFSAutoDeployService {
 
     async checkForIPFSDeplymentUpdates() {
         // Check for IPFS Deployment Updates with Github API
+
+        const octokit = new Octokit({
+            auth: process.env.GITHUB_AUTH_TOKEN,
+          })
+          
+        const response = await octokit.request('GET /repos/persistenceOne/persistenceWallet/releases', {
+            owner: 'persistenceOne',
+            repo: 'persistenceWallet',
+            headers: {
+              // 'Accept': 'application/vnd.github.v3+json',
+              'X-GitHub-Api-Version': '2022-11-28'
+            }
+          })
+        
+          const latestRelease = response.data;
+        
+          return latestRelease;
 
         // Compare the latest IPFS Deployment with the current IPFS Deployment in the database
 
